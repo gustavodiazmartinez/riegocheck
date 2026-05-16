cb657265653ad60ff85e90159a1157472a1355bb
gustavodiazmartinez <gusdiazmar65@gmail.com>
2026-05-16 12:36:38 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'riegocheck-v1';
+const urlsToCache = [
+  '/',
+  '/riegocheck_v3.html',
+  '/manifest.json',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
